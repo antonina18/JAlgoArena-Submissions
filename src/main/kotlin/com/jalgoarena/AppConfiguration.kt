48f384410c832de04ef2f0ddf4e89c00d158efba
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-11-26 16:11:40 +0100

    add swagger

--- src/main/kotlin/com/jalgoarena/AppConfiguration.kt
+++ src/main/kotlin/com/jalgoarena/AppConfiguration.kt
@@ -1,14 +1,19 @@
 package com.jalgoarena
 
 import com.jalgoarena.data.SubmissionsRepository
-import com.jalgoarena.ranking.*
+import com.jalgoarena.ranking.BasicRankingCalculator
+import com.jalgoarena.ranking.BasicScoreCalculator
+import com.jalgoarena.ranking.BonusPointsForBestTimeRankingCalculator
+import com.jalgoarena.ranking.RankingCalculator
 import com.jalgoarena.web.ProblemsClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestOperations
 import org.springframework.web.client.RestTemplate
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
+@EnableSwagger2
 open class AppConfiguration {
 
     @Bean
